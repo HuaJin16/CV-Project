f113ad7c03ad99f6ee8f6f160bc16723eae67299
Hua Jin <huazjin16@gmail.com>
2023-12-17 23:09:42 -0600

    added functionality to basic information and professional experience components

--- CV-Application/src/components/BasicInfo.jsx
+++ CV-Application/src/components/BasicInfo.jsx
@@ -1,24 +1,99 @@
 import React from "react";
+import { useState } from "react";
 
 function BasicInfo() {
+  const initialValues = {
+    name: "",
+    email: "",
+    number: "",
+    address: "",
+  };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [status, setStatus] = useState("pending");
+
+  //edits the value of each form component
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  //edits the visibility of form components
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (status == "pending") {
+      setStatus("submitted");
+    } else if (status == "submitted") {
+      setStatus("editing");
+    } else if (status == "editing") {
+      setStatus("submitted");
+    }
+  };
+
   return (
     <>
-      <h1>Basic Information</h1>
-      <form>
-        <label>Full Name</label>
-        <input key="name" type="text" />
-        <label>Email</label>
-        <input key="email" type="text" />
-        <label>Phone Number</label>
-        <input key="number" type="text" />
-        <label>Address</label>
-        <input key="address" type="text" />
-        <div>
-          <button>Submit</button>
-        </div>
-        <div>
-          <button>Edit</button>
-        </div>
+      <form onSubmit={handleSubmit}>
+        <h1>Basic Information</h1>
+        {(status == "pending" || status == "editing") && (
+          <>
+            {" "}
+            <div className="formComponent">
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter Name"
+                value={formValues.name}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formComponent">
+              <label>Email</label>
+              <input
+                type="text"
+                name="email"
+                placeholder="Enter Email"
+                value={formValues.email}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formComponent">
+              <label>Phone Number</label>
+              <input
+                type="text"
+                name="number"
+                placeholder="Enter Phone Number"
+                value={formValues.number}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formComponent">
+              <label>Address</label>
+              <input
+                type="text"
+                name="address"
+                placeholder="Enter Address"
+                value={formValues.address}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <button>Save</button>
+            </div>
+          </>
+        )}
+
+        {status == "submitted" && (
+          <>
+            {" "}
+            <div>
+              <button>Edit</button>
+            </div>
+          </>
+        )}
       </form>
     </>
   );
--- CV-Application/src/components/Professional.jsx
+++ CV-Application/src/components/Professional.jsx
@@ -1,26 +1,111 @@
 import React from "react";
+import { useState } from "react";
 
 function Professional() {
+  const initialValues = {
+    title: "",
+    company: "",
+    description: "",
+    start: "",
+    end: "",
+  };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [status, setStatus] = useState("pending");
+
+  //edits the value of each form component
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  //edits the visibility of form components
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (status == "pending") {
+      setStatus("submitted");
+    } else if (status == "submitted") {
+      setStatus("editing");
+    } else if (status == "editing") {
+      setStatus("submitted");
+    }
+  };
+
   return (
     <>
-      <h1>Professional Experience</h1>
-      <form>
-        <label>Job Title</label>
-        <input key="title" type="text" />
-        <label>Company</label>
-        <input key="company" type="text" />
-        <label>Description</label>
-        <input key="description" type="text" />
-        <label>Start Date</label>
-        <input key="start" type="text" />
-        <label>End</label>
-        <input key="end" type="text" />
-        <div>
-          <button>Submit</button>
-        </div>
-        <div>
-          <button>Edit</button>
-        </div>
+      <form onSubmit={handleSubmit}>
+        <h1>Professional Experience</h1>
+        {(status == "pending" || status == "editing") && (
+          <>
+            {" "}
+            <div className="formComponent">
+              <label>Job Title</label>
+              <input
+                type="text"
+                name="title"
+                placeholder="Enter Job Title"
+                value={formValues.title}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formComponent">
+              <label>Company</label>
+              <input
+                type="text"
+                name="company"
+                placeholder="Enter Company"
+                value={formValues.company}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formComponent">
+              <label>Description</label>
+              <input
+                type="text"
+                name="description"
+                placeholder="Enter Job Description"
+                value={formValues.description}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formComponent">
+              <label>Start Date</label>
+              <input
+                type="text"
+                name="start"
+                placeholder="Enter Start Date"
+                value={formValues.start}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="formComponent">
+              <label>End Date</label>
+              <input
+                type="text"
+                name="end"
+                placeholder="Enter End Date"
+                value={formValues.end}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <button>Save</button>
+            </div>
+          </>
+        )}
+
+        {status == "submitted" && (
+          <>
+            {" "}
+            <div>
+              <button>Edit</button>
+            </div>
+          </>
+        )}
       </form>
     </>
   );
